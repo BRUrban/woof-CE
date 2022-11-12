7faed48259a6812777805a4e8969c6cac16c5851
Brian Urban <brurban@gmail.com>
2022-11-11 22:39:51 -0800

    Add logic to ensure ethernet comes up before barrier

--- woof-code/rootfs-skeleton/root/Startup/barrier_setup.sh
+++ woof-code/rootfs-skeleton/root/Startup/barrier_setup.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+##This part just ensures ethernet comes up automatically
+# Find our NIC device.
+#
+NIC=$(ifconfig  -a | grep eth|awk {'print $1'}) # gives us lowest numbered eth? adapter
+
+# Bounce NIC device
+ifconfig $NIC down
+ifconfig $NIC up
+
+##This part runs the actual barrier setup stuff
+# Generate appropriate-format fingerprints
 $HOME/.barrier/barrier-trust.sh
+
+# Start barrier client
 $HOME/.barrier/barrier-client.sh
